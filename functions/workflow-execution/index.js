8b5501c405c9c6f43badeb8e9d63c9ba77e1869c
Nithin Brammesh <brammeshnithin@gmail.com>
2026-08-10 23:00:29 +0530

    Fix workflow execution CORS

--- functions/workflow-execution/index.js
+++ functions/workflow-execution/index.js
@@ -6,7 +6,27 @@ const { getAuthenticatedUserId } = require("./auth");
 // Nhost:
 // POST /v1/functions/workflow-execution
 
+
+
+
 module.exports = async (req, res) => {
+
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "POST, OPTIONS"
+  );
+
+  if (req.method === "OPTIONS") {
+    return res.status(204).end();
+  }
+
+
+
   if (req.method !== "POST") {
     return res.status(405).json({
       error: "Method not allowed, use POST",
